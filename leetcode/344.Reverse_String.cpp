48373391ed98a3167e190edcf249bb0a81991023
Phạm Thanh Hùng <hungthanhpham94@gmail.com>
2020-06-04 21:12:11 +0700

    Create 344.Reverse_String.cpp

---
+++ leetcode/344.Reverse_String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        char c;
+        for (int i = 0; i < int(n / 2); ++i) {
+            c = s[i];
+            s[i] = s[n - i - 1];
+            s[n - i - 1] = c;
+        }
+    }
+};
