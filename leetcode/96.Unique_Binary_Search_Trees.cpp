1db909fe1ea3318e23f3f69fb78939da391b48dd
Phạm Thanh Hùng <hungthanhpham94@gmail.com>
2020-06-26 17:58:35 +0700

    Create 96.Unique_Binary_Search_Trees.cpp

---
+++ leetcode/96.Unique_Binary_Search_Trees.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        int dp[n + 5];
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = 0;
+            for (int j = 1; j <= i; ++j) dp[i] += dp[j - 1] * dp[i - j];
+        }
+        return dp[n];
+    }
+};
