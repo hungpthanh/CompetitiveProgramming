dc7703b6562c2627addfbcc022e897d80778e7a0
Phạm Thanh Hùng <hungthanhpham94@gmail.com>
2020-08-09 00:09:09 +0700

    Create 21.Merge_Two_Sorted_Lists.cpp

---
+++ leetcode/21.Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
+        if (l1->val < l2->val) return new ListNode(l1->val, mergeTwoLists(l1->next, l2));
+        return new ListNode(l2->val, mergeTwoLists(l1, l2->next));
+    }
+};
