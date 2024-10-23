b4e91dd0e310cf1efea8de4aefe68db31de14405
Pham Thanh Hung <hungthanhpham94@gmail.com>
2024-10-24 01:07:38 +0900

    Update 143.Reorder_List.py

--- leetcode/143.Reorder_List.py
+++ leetcode/143.Reorder_List.py
@@ -3,24 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# TLE
 class Solution(object):
     def reorderList(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: None Do not return anything, modify head in-place instead.
         """
-        if head is None:
-            return head
-        if head.next is None:
-            return head
-        current = head
-        while (current.next is not None):
-            # print(current.val)
-            prev = current
-            current = current.next
-        prev.next = None
-        current.next = self.reorderList(head.next)
-        head.next = current
-        return head
+        Q = deque()
+        cnt = 0
+        while (head.next is not None):
+            Q.append(head)
+            head = head.next
+        Q.append(head)
+        head = ListNode(0, None)
+        while (len(Q) > 1):
+            head.next = Q[0]
+            head.next.next = Q[-1]
+            Q.pop()
+            Q.popleft()
+            head = head.next.next
+        if len(Q) == 1:
+            head.next = Q[0]
+            head = head.next
+        head.next = None
+        head = head.next
+
+
         
