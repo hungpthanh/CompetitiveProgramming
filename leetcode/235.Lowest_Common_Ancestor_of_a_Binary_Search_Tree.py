96d8a1ce3fdba7a72f2542a16a4a35fd7768f36a
Pham Thanh Hung <hungthanhpham94@gmail.com>
2024-10-26 18:43:24 +0900

    Create 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py

---
+++ leetcode/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        min_v = min(p.val, q.val)
+        max_v = max(p.val, q.val)
+        if (min_v <= root.val <= max_v): return root
+        if root.val > max_v:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
