2a55001c7761a0ea4a0b9b3f6d2f20d331da6fd1
Pham Thanh Hung <hungthanhpham94@gmail.com>
2024-11-16 09:50:27 +0900

    Update 212.Word_Search_II.py

--- leetcode/212.Word_Search_II.py
+++ leetcode/212.Word_Search_II.py
@@ -2,73 +2,100 @@ class Node:
     def __init__(self, data=None):
         self.data: str = data
         self.childs: List['Node'] = []
+        self.is_end = False
+        self.parent = None
 
-    def add(self, c: str):
+    def add_word(self, word: str) -> None:
+        if len(word) == 0:
+            self.is_end = True
+            return
         for child in self.childs:
-            if child.data == c:
-                return child
-        new_child = Node(c)
+            if child.data == word[0]:
+                child.add_word(word[1:])
+                return
+        new_child = Node(word[0])
+        new_child.parent = self
         self.childs.append(new_child)
-        return new_child
+        new_child.add_word(word[1:])
+
+    def delete(node):
+        parent = node.parent
+        if parent:
+            while (len(parent.childs)) < 2:
+                parent.childs.remove(node)
+                node = None
+                delete(parent)
 
     def search(self, word: str) -> bool:
         if not word:
-            return True
+            return True, self.is_end
         for child in self.childs:
-            if child.data == word[0]:
-                found = child.search(word[1:])
-                return found
-        return False
+            if child.data ==  word[0]:
+                found, is_end = child.search(word[1:])
+                return found, is_end
+        return False, False
 
-    def __str__(self, level=0) -> str:
-        result = "  " * level + (self.data if self.data else "Root") + ("\n" if level else "")
+    def search_char(self, c: str) -> bool:
         for child in self.childs:
-            result += child.__str__(level + 1)
-        return result
+            if child.data ==  c:
+                return child
+        return None
+
+
 
 class Solution:
 
-    def build_multiple_tree(self, board: List[List[str]]) -> None:
+    def build_multiple_tree(self, board: List[List[str]], words) -> None:
+        query_trie = Node("root")
+        for word in words:
+            query_trie.add_word(word)
+        # print(query_trie)
         m = len(board)
         n = len(board[0])
-        M = {}
         mark = [[False] * n for _ in range(m)]
         cx = [-1, 1, 0, 0]
         cy = [0, 0, -1, 1]
-        # ss = ""
-        # built_words = []
-        def dfs(i: int, j: int, node: 'Node', cnt: int) -> None:
+        results = set()
+        tmp = []
+        def dfs(i: int, j: int, cnt: int, node) -> None:
+            nonlocal tmp            
             cnt += 1
             if cnt > 10:
-                # built_words.append(ss)
                 return
-            next_node = node.add(board[i][j])
+            if node.is_end:
+                results.add("".join(tmp))
+            # next_child, is_end = query_trie.search(board[i][j])
+            
+            # if not next_child and is_end:
+            #     results.append(ss)
+
             for k in range(4):
                 x = i + cx[k]
                 y = j + cy[k]
-                if (0 <= x < m) and (0 <= y < n) and (not mark[x][y]):
+                if not ((0 <= x < m) and (0 <= y < n)):
+                    continue
+                if mark[x][y]:
+                    continue
+                next_node = node.search_char(board[x][y])
+                if next_node:
                     mark[x][y] = True
-                    # ss = ss + board[x][y]
-                    dfs(x, y, next_node, cnt)
-                    # ss = ss[0:-1]
+                    tmp.append(board[x][y])
+                    dfs(x, y, cnt, next_node)
                     mark[x][y] = False
+                    tmp = tmp[:-1]
         mark = [[False] * n for _ in range(m)]
 
         for i in range(m):
             for j in range(n):
-                mark[i][j] = True
-                M[(i, j)] = Node("root")
-                dfs(i, j, M[(i, j)], 0)
-                mark[i][j] = False
-        return M
+                next_node = query_trie.search_char(board[i][j])
+                if next_node:
+                    mark[i][j] = True
+                    tmp.append(board[i][j])
+                    dfs(i, j, 0, next_node)    
+                    tmp = []
+                    mark[i][j] = False
+        return results
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        M = self.build_multiple_tree(board)
-        results = []
-        for word in words:
-            for trie in M.values():
-                found = trie.search(word)
-                if found:
-                    results.append(word)
-                    break
+        results = list(self.build_multiple_tree(board, words))
         return results
