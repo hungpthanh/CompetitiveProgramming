94271899c111d9b91a861d9954acdff6b79eee57
Pham Thanh Hung <hungthanhpham94@gmail.com>
2024-12-20 23:10:42 +0900

    Update 371.Sum_of_Two_Integers.py

--- leetcode/371.Sum_of_Two_Integers.py
+++ leetcode/371.Sum_of_Two_Integers.py
@@ -1,1 +1,11 @@
-# TODO
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        max_int = 0x7FFFFFFF
+
+        while b != 0:
+            carry = (a & b) << 1
+            a = (a ^ b) & mask  # keep 32-bit format
+            b = carry & mask # keep 32-bit format
+
+        return a if a <= max_int else ~(a ^ mask) # two complement
