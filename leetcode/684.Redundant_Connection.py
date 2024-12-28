dd2e7601ddcdbe85ff005ddd24f6e6f59d5d5ddb
Pham Thanh Hung <hungthanhpham94@gmail.com>
2024-12-28 23:33:50 +0900

    Update 684.Redundant_Connection.py

--- leetcode/684.Redundant_Connection.py
+++ leetcode/684.Redundant_Connection.py
@@ -1,1 +1,24 @@
-# TODO
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = {i: [] for i in range(1, n + 1)}
+        for (u, v) in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visited = set()
+        results = None
+        def dfs(u):
+            nonlocal results
+            if results is not None:
+                return
+            for v in adj[u]:
+                if v in visited:
+                    if [u, v] in edges:
+                        results = [u, v]
+                    else:
+                        results = [v, u]
+                else:
+                    visited.add(v)
+                    dfs(v)
+        visited.add(1)
+        dfs(1)
