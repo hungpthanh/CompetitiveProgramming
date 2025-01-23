c265c577be834c2a3d57a4706b2e106d115cb9d8
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-01-23 23:31:13 +0700

    Update 115.Distinct_Subsequences.py

--- leetcode/115.Distinct_Subsequences.py
+++ leetcode/115.Distinct_Subsequences.py
@@ -1,1 +1,14 @@
-# TODO
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        s, t = "#" + s, "#" + t
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+
+        for i in range(1, n + 1):
+            dp[i][0] = 1
+            for j in range(1, m + 1):
+                dp[i][j] = dp[i - 1][j]
+                if s[i] == t[j]:
+                    dp[i][j] += dp[i - 1][j - 1]
+        return dp[n][m]
