7e4c4f63c515053e7388212108a56fe881ff35a2
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-01-27 22:46:40 +0700

    Update 567.Permutation_in_String.py

--- leetcode/567.Permutation_in_String.py
+++ leetcode/567.Permutation_in_String.py
@@ -2,21 +2,16 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2) < len(s1):
             return False
-        count_s1 = Counter(s1)
-        count_s2 = Counter(s2[:len(s1)])
-        if len(s1) == len(s2):
-            return count_s1 == count_s2
-        index = len(s1)
-        while True:
-            if count_s1 == count_s2:
+        count1 = Counter(s1)
+        count2 = Counter(s2[:len(s1)])
+        if count1 == count2:
+            return True
+        for i in range(len(s1), len(s2)):
+            count2.setdefault(s2[i], 0)
+            count2[s2[i]] += 1
+            count2[s2[i - len(s1)]] -= 1
+            if count2[s2[i - len(s1)]] == 0:
+                count2.pop(s2[i - len(s1)])
+            if count2 == count1:
                 return True
-            if index >= len(s2):
-                break
-            if s2[index] not in count_s2:
-                count_s2[s2[index]] = 0
-            count_s2[s2[index]] += 1
-            count_s2[s2[index - len(s1)]] -= 1
-            if count_s2[s2[index - len(s1)]] == 0:
-                count_s2.pop(s2[index - len(s1)])
-            index += 1
         return False
