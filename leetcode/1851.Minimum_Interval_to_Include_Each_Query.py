419b17a0e82b2291f9cd3ba8649e4266cfa0bb3d
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-02-05 22:43:39 +0700

    Update 1851.Minimum_Interval_to_Include_Each_Query.py

--- leetcode/1851.Minimum_Interval_to_Include_Each_Query.py
+++ leetcode/1851.Minimum_Interval_to_Include_Each_Query.py
@@ -1,1 +1,29 @@
-# TODO
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        data = []
+        for interval in intervals:
+            data.append((interval[0], -1, interval[1] - interval[0] + 1))
+            data.append((interval[1], 1, interval[1] - interval[0] + 1))
+        for idx, query in enumerate(queries):
+            data.append((query, 0, idx))
+        data.sort(key=lambda x: (x[0], x[1]))
+
+        heap = []
+        heapq.heapify(heap)
+        count = defaultdict(int)
+        ans = [0] * len(queries)
+        for item in data:
+            point, tp, value = item
+            if tp == -1:
+                if count[value] == 0:
+                    heapq.heappush(heap, value)
+                count[value] += 1
+            if tp == 1:
+                if count[value] > 0:
+                    count[value] -= 1
+            if tp == 0:
+                while heap and count[heap[0]] == 0:
+                    heapq.heappop(heap)
+                ans[value] = heap[0] if len(heap) > 0 else -1
+                
+        return ans
