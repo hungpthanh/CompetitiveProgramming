8325409a3e7b7a202456e143c1a5bcf294700111
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-02-26 23:44:40 +0700

    Update 875.Koko_Eating_Bananas.py

--- leetcode/875.Koko_Eating_Bananas.py
+++ leetcode/875.Koko_Eating_Bananas.py
@@ -1,1 +1,17 @@
-# TODO
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def check(k):
+            t = 0
+            for pile in piles:
+                t += math.ceil(pile / k)
+            return t <= h
+        l, r = 1, int(1e9 + 5)
+        ans = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if check(mid):
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return ans
