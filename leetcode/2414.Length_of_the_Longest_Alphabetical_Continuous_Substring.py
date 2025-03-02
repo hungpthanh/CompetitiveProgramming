2e4f3fbc446785b246afd5a64fae1acf7d75784d
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-03-02 23:06:16 +0700

    Update 2414.Length_of_the_Longest_Alphabetical_Continuous_Substring.py

--- leetcode/2414.Length_of_the_Longest_Alphabetical_Continuous_Substring.py
+++ leetcode/2414.Length_of_the_Longest_Alphabetical_Continuous_Substring.py
@@ -1,1 +1,16 @@
-# TODO
+class Solution:
+    def longestContinuousSubstring(self, s: str) -> int:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        start = 0
+        n = len(s)
+        res = 0
+        for i in range(n):
+            index = ord(s[start]) - ord('a')
+            if index + i - start >= len(alphabet):
+                start = i
+                continue
+            if s[i] == alphabet[index + i - start]:
+                res = max(res, i - start + 1)
+            else:
+                start = i
+        return res
