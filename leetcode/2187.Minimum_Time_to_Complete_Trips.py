326e4833f1ff9bb7df49285e414a3a835a4382bc
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-03-09 13:22:26 +0700

    Update 2187.Minimum_Time_to_Complete_Trips.py

--- leetcode/2187.Minimum_Time_to_Complete_Trips.py
+++ leetcode/2187.Minimum_Time_to_Complete_Trips.py
@@ -1,1 +1,17 @@
-# TODO
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        l, r = 1, max(time) * totalTrips + 5
+        def check(ti):
+            trip = 0
+            for t in time:
+                trip += ti // t
+            return trip
+        ans = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if check(mid) >= totalTrips:
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return ans
