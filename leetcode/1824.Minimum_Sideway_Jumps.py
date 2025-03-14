85d7eb2d483c0c6c4b129432df6ad65c7e2733d7
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-03-14 21:23:54 +0700

    Update 1824.Minimum_Sideway_Jumps.py

--- leetcode/1824.Minimum_Sideway_Jumps.py
+++ leetcode/1824.Minimum_Sideway_Jumps.py
@@ -2,7 +2,7 @@ class Solution:
     def minSideJumps(self, obstacles: List[int]) -> int:
         n = len(obstacles) - 1
         inf = n + 5
-        dp = [[0] * 4 for _ in range(n + 1)]
+        dp = [[inf] * 4 for _ in range(n + 1)]
         dp[0][2] = 0
         dp[0][1] = 1 if obstacles[0] != 1 else inf
         dp[0][3] = 1 if obstacles[0] != 3 else inf
@@ -12,10 +12,7 @@ class Solution:
                     dp[i][j] = inf
                 else:
                     dp[i][j] = dp[i - 1][j]
-                    if obstacles[i] != 1:
-                        dp[i][j] = min(dp[i][j], dp[i - 1][1] + 1)
-                    if obstacles[i] != 2:
-                        dp[i][j] = min(dp[i][j], dp[i - 1][2] + 1)
-                    if obstacles[i] != 3:
-                        dp[i][j] = min(dp[i][j], dp[i - 1][3] + 1)
+                    for k in range(1, 4):
+                        if obstacles[i] != k:
+                            dp[i][j] = min(dp[i][j], dp[i - 1][k] + 1)
         return min(dp[n][1:])
