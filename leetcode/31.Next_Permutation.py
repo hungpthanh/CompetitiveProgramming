2151f0e0732777015fc0a9d52b04689d3d8494c6
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-03 23:46:29 +0900

    Update 31.Next_Permutation.py

--- leetcode/31.Next_Permutation.py
+++ leetcode/31.Next_Permutation.py
@@ -5,4 +5,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n = len(nums)
+        st = n - 1
+        while (st >= 1) and (nums[st] <= nums[st - 1]):
+            st -= 1
+        if st != 0:
+            pos = st
+            for i in range(st, n):
+                if nums[i] > nums[st - 1] and nums[i] < nums[pos]:
+                    pos = i
+            nums[st - 1], nums[pos] = nums[pos], nums[st - 1]
+        for i in range(st, n - 1):
+            for j in range(i + 1, n):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
         
