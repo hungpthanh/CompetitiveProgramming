ac7de780d701fb67568e051212d17d84103888a7
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-15 23:34:00 +0900

    Update 780.Reaching_Points.py

--- leetcode/780.Reaching_Points.py
+++ leetcode/780.Reaching_Points.py
@@ -1,4 +1,5 @@
 # IDEA: The operations are Euclean GCD algorithm
+# Gradually, sub value to each start pair
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
         
