d69811d4a2bd69268c113c38dbb20562a28dafbd
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-16 23:15:14 +0900

    Update 780.Reaching_Points.py

--- leetcode/780.Reaching_Points.py
+++ leetcode/780.Reaching_Points.py
@@ -1,5 +1,6 @@
 # IDEA: The operations are Euclean GCD algorithm
 # Gradually, sub value to each start pair
+# from (a, b) -> (c, d) then have multile (c, d) but from a (c, d) -> (a, b) only one path
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
         
