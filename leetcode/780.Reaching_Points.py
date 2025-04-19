4bbe87ed9f2c24a823be0d81af65e952d8f426d5
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-19 14:46:48 +0900

    Update 780.Reaching_Points.py

--- leetcode/780.Reaching_Points.py
+++ leetcode/780.Reaching_Points.py
@@ -3,4 +3,16 @@
 # from (a, b) -> (c, d) then have multile (c, d) but from a (c, d) -> (a, b) only one path
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+
+        def gcd(sx: int, sy: int, tx: int, ty: int) -> bool:
+            if not (sx <= tx and sy <= ty):
+                return False
+            if tx < ty:
+                return gcd(sy, sx, ty, tx)
+            if tx % ty >= sx:
+                return gcd(sx, sy, tx % ty, ty)
+            if (tx - sx) % ty == 0:
+                return ty == sy
+            return False
+        return gcd(sx, sy, tx, ty)
         
