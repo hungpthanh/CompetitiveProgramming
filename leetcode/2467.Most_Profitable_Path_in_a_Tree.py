72f04297561ddf3649f1253db6562776bab0897c
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-24 23:39:11 +0900

    Update 2467.Most_Profitable_Path_in_a_Tree.py

--- leetcode/2467.Most_Profitable_Path_in_a_Tree.py
+++ leetcode/2467.Most_Profitable_Path_in_a_Tree.py
@@ -6,37 +6,45 @@ class Solution:
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-        parent = [-1 for _ in range(n)]
+
+        parent = [(-1, 0) for _ in range(n)]
         
         childs = [[] for _ in range(n)]
         def build_tree(u, level):
             for v in adj[u]:
-                if v != 0 and parent[v] == -1:
+                if v != 0 and parent[v][0] == -1:
                     parent[v] = (u, level + 1)
                     childs[u].append(v)
                     build_tree(v, level + 1)
+        build_tree(0, 0)
+            
         level = 0
         t = [-1 for _ in range(n)]
-        while parent[bob] != -1:
+        while parent[bob][0] != -1:
             t[bob] = level
             level += 1
-            bob = parent[bob]
+            bob = parent[bob][0]
         
         dp = [0 for _ in range(n)]
-        mark = [False for _ in range(n)]
+        dp[0] = amount[0] if t[0] != 0 else amount[0] // 2
         def dfs(u):
-            mark[u] = True
             for v in childs[u]:
-                if not mark[v]:
-                    dp[v] = dp[u] + amount[v] if (parent[v][1] != t[v]) else amount[v] / 2
-                    dfs(v)
+                if t[v] != -1:
+                    if t[v] == parent[v][1]:
+                        dp[v] = dp[u] + amount[v] // 2
+                    elif parent[v][1] > t[v]:
+                        dp[v] = dp[u]
+                    else:
+                        dp[v] = dp[u] + amount[v]
+                else:
+                    dp[v] = dp[u] + amount[v]
+                dfs(v)
         dfs(0)
         ans = -1e18
         for u in range(n):
             if len(childs[u]) == 0:
                 ans = max(ans, dp[u])
         return ans
-
 
 
 
