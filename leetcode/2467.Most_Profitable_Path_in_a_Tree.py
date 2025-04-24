b8bc319b5edc4eeac74b55d51c10b77eee04a306
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-24 23:11:27 +0900

    Update 2467.Most_Profitable_Path_in_a_Tree.py

--- leetcode/2467.Most_Profitable_Path_in_a_Tree.py
+++ leetcode/2467.Most_Profitable_Path_in_a_Tree.py
@@ -1,4 +1,45 @@
 # IDEA: TBD
 class Solution:
     def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
+        n = len(edges) + 1
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        parent = [-1 for _ in range(n)]
+        
+        childs = [[] for _ in range(n)]
+        def build_tree(u, level):
+            for v in adj[u]:
+                if v != 0 and parent[v] == -1:
+                    parent[v] = (u, level + 1)
+                    childs[u].append(v)
+                    build_tree(v, level + 1)
+        level = 0
+        t = [-1 for _ in range(n)]
+        while parent[bob] != -1:
+            t[bob] = level
+            level += 1
+            bob = parent[bob]
+        
+        dp = [0 for _ in range(n)]
+        mark = [False for _ in range(n)]
+        def dfs(u):
+            mark[u] = True
+            for v in childs[u]:
+                if not mark[v]:
+                    dp[v] = dp[u] + amount[v] if (parent[v][1] != t[v]) else amount[v] / 2
+                    dfs(v)
+        dfs(0)
+        ans = -1e18
+        for u in range(n):
+            if len(childs[u]) == 0:
+                ans = max(ans, dp[u])
+        return ans
+
+
+
+
+
+        
         
