88c330a62d00cf1b313d3566ac07efa276815b29
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-25 23:07:43 +0900

    Update 781.Rabbits_in_Forest.py

--- leetcode/781.Rabbits_in_Forest.py
+++ leetcode/781.Rabbits_in_Forest.py
@@ -1,1 +1,14 @@
-# TODO
+# IDEA: TBD
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        M = Counter(answers)
+        cnt = 0
+        while len(M) > 0:
+            first_key = next(iter(M))
+            value = M[first_key]
+            if value > first_key + 1:
+                M[first_key] = value - first_key - 1
+            else:
+                del M[first_key]
+            cnt += (first_key + 1)
+        return cnt
