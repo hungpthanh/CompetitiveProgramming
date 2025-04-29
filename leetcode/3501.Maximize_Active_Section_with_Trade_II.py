72b0af9d936d9b57231792927c95abe2e1b63b7d
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-04-29 22:05:06 +0900

    Update 3501.Maximize_Active_Section_with_Trade_II.py

--- leetcode/3501.Maximize_Active_Section_with_Trade_II.py
+++ leetcode/3501.Maximize_Active_Section_with_Trade_II.py
@@ -1,12 +1,12 @@
 class Solution:
-    def maxActiveSectionsAfterTrade(self, s: str, queries: List[List[int]]) -> List[int]:
+
+    def create_segments(s):
         st, en = -1, -1
         n = len(s)
         segments = []
         print(f"n = {n}")
         fsum = [0] * n
-        # if s[0] == '1':
-        #     fsum[0] = 1 
+ 
         print(fsum)
         for i in range(n):
             if i > 0:
@@ -27,47 +27,17 @@ class Solution:
         
         if st != -1:
             segments.append([st, en])
-        print(segments)
-        m = len(segments)
-        def find_right(m, k):
-            print(f"right: {m} {k}")
-            l, r = 0, m - 1
-            ans = -1
-            while l <= r:
-                mid = (l + r) // 2
-                print(f"mid: {mid}, segments: {segments[mid]}")
-                if segments[mid][1] < k:
-                    ans = mid
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return ans
+        
+        return segments
 
-        def find_left(m, k):
-            l, r = 0, m - 1
-            ans = -1
-            while l <= r:
-                mid = (l + r) // 2
-                if segments[mid][0] > k:
-                    ans = mid
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return ans
-        res = []
-        print("fsum")
-        print(fsum)
-        for q_st, q_en in queries:
-            print(f"query: {q_st} {q_en}")
-            index1 = find_left(m, q_st)
-            index2 = find_right(m, q_en)
-            print(f"index: {index1} {index2}")
-            ans = (fsum[q_st - 1] if q_st - 1 >= 0 else 0) + fsum[n - 1] - fsum[q_en]
-            print(f"ans = {ans}")
-            if index1 != -1 and index2 != -1 and index1 <= index2:
-                ans += q_en - q_st + 1
-            else:
-                ans = fsum[n - 1]
-            print(f"final ans = {ans}")
-            res.append(ans)
+    def maxActiveSectionsAfterTrade(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(s)
+        segments = create_segments()
+        m = len(segments)
+        
+        actives = [0] * m
+        for i in range(m):
+            p = segments[i - 1][1] if i - 1 >= 0 else -1
+            q = segments[i + 1][1] if i + 1 < m else n
+            actives[i] = q - p
         return res
