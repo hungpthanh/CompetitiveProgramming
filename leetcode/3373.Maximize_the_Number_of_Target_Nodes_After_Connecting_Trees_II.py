3b00e20670f9f4bdd426e773600fcdb45e72fa25
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-06-01 20:59:40 +0900

    Update 3373.Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_II.py

--- leetcode/3373.Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_II.py
+++ leetcode/3373.Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_II.py
@@ -1,4 +1,5 @@
 # SOL: color graph
+# Hint: u target to v then number of node target u = number of node target v
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
         
