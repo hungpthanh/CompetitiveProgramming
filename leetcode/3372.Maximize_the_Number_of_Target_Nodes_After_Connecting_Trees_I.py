7f84c9c85a0ff81060b7fe2fc8434d81dff4fb2f
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-06-06 04:33:27 +0900

    Update 3372.Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_I.py

--- leetcode/3372.Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_I.py
+++ leetcode/3372.Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_I.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
-        n = len(edges1) + 1
-        m = len(edges2) + 1
+        n, m = len(edges1) + 1, len(edges2) + 1
 
         def build_adj(n, edges):
             adj = {i: [] for i in range(n)}
@@ -13,22 +12,15 @@ class Solution:
         adj1 = build_adj(n, edges1)
         adj2 = build_adj(m, edges2)
 
-        def buid_distance(u, parent_u, adj, level, max_level):
+        def build_distance(u, parent_u, adj, level, max_level):
             if level > max_level:
                 return 0
             cnt = 1
             for v in adj[u]:
                 if v != parent_u:
-                    cnt += buid_distance(v, u, adj, level + 1, max_level)
+                    cnt += build_distance(v, u, adj, level + 1, max_level)
             return cnt
-        max_on_tree2 = -1
-        for u in range(m):
-            cnt = buid_distance(u, -1, adj2, 0, k - 1)
-            max_on_tree2 = max(max_on_tree2, cnt)
-        res = 0
-        ans = []
-        for i in range(n):
-            cnt1 = buid_distance(i, -1, adj1, 0, k)
-            res = cnt1 + max_on_tree2
-            ans.append(res)
+
+        max_on_tree2 = max([build_distance(u, -1, adj2, 0, k - 1) for u in range(m)])
+        ans = [build_distance(i, -1, adj1, 0, k) + max_on_tree2 for i in range(n)]
         return ans
