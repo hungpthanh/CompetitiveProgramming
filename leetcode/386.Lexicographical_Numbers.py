78e7d35727239ef993e20b6df2e129bffd463ae0
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-06-10 23:05:41 +0900

    Update 386.Lexicographical_Numbers.py

--- leetcode/386.Lexicographical_Numbers.py
+++ leetcode/386.Lexicographical_Numbers.py
@@ -1,22 +1,12 @@
-# Poor my code :(
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        m = len(str(n))
         ans = []
-        r = [""] + [str(k) for k in range(10)]
-        tmp = ""
-
-        def backtrack(i):
-            nonlocal tmp
-            if i == m:
-                if (len(tmp) > 0) and int(tmp) <= n:
-                    ans.append(tmp)
+        def dfs(number):
+            if number != "" and int(number) > n:
                 return
-            st = 2 if i == 0 else 0
-            for j in r[st:]:    
-                tmp = tmp + j
-                backtrack(i + 1)
-                if j != "":
-                    tmp = tmp[:-1]
-        backtrack(0)
-        return [int(x) for x in ans if int(x) > 0]
+            ans.append(number)
+            st = 1 if number == "" else 0
+            for c in range(st, 10):
+                dfs(number + str(c))
+        dfs("")
+        return [int(item) for item in ans[1:]]
