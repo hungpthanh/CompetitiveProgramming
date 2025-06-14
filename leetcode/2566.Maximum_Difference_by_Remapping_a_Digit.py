1babc59ed6986549281c38b55ed1bd8160d8a41a
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-06-14 22:34:08 +0900

    Update 2566.Maximum_Difference_by_Remapping_a_Digit.py

--- leetcode/2566.Maximum_Difference_by_Remapping_a_Digit.py
+++ leetcode/2566.Maximum_Difference_by_Remapping_a_Digit.py
@@ -6,20 +6,11 @@ class Solution:
         else:
             min_value = str(num)
             st = min_value[0]
-            min_value = min_value.replace(st, '0')
-            min_value = int(min_value)
+            min_value = int(min_value.replace(st, '0'))
         max_value = str(num)
         st = 0
         while st < len(max_value) and max_value[st] == '9':
             st += 1
-        if st == len(max_value):
-            max_source = '9'
-            max_target = '9'
-        else:
-            max_source = max_value[st]
-            max_target = '9'
-        max_value = max_value.replace(max_source, max_target)
-        max_value = int(max_value)
+        max_source = '9' if st == len(max_value) else max_value[st]
+        max_value = int(max_value.replace(max_source, '9'))
         return max_value - min_value
-
-        
