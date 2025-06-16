16f2e6299e785665bbcd717055adaa5eb0ae7e99
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-06-16 21:45:27 +0900

    Update 3203.Find_Minimum_Diameter_After_Merging_Two_Trees.py

--- leetcode/3203.Find_Minimum_Diameter_After_Merging_Two_Trees.py
+++ leetcode/3203.Find_Minimum_Diameter_After_Merging_Two_Trees.py
@@ -1,4 +1,5 @@
 # IDEA: TBD
+# Resource: https://codeforces.com/blog/entry/101271
 class Solution:
     def minimumDiameterAfterMerge(self, edges1: List[List[int]], edges2: List[List[int]]) -> int:
         
