c02bce8e48b5099a04884d5a847cf338436f0898
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-06-27 23:23:23 +0900

    Update 2040.Kth_Smallest_Product_of_Two_Sorted_Arrays.py

--- leetcode/2040.Kth_Smallest_Product_of_Two_Sorted_Arrays.py
+++ leetcode/2040.Kth_Smallest_Product_of_Two_Sorted_Arrays.py
@@ -1,5 +1,24 @@
 class Solution:
     def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        MAX_INT = int(1e10 + 5)
+        def gen(a, b):
+            n, m = len(a), len(b)
+            i, j = 0, 0
+            while (i < n) and (j < m):
+                yield a[i] * b[j]
+                if i == n - 1 and j == m - 1:
+                    break
+                if i == n - 1:
+                    j += 1
+                    continue
+                if j == m - 1:
+                    i += 1
+                    continue
+                if (a[i + 1] * b[j] < a[i] * b[j + 1]):
+                    i += 1
+                else:
+                    j += 1
+            
         n, m = len(nums1), len(nums2)
         i, j = 0, 0
         cnt = 0
