9e20722f79f280c6583de8b5e704c6bbf370ab37
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-07-12 05:43:59 +0900

    Update 1790.Check_if_One_String_Swap_Can_Make_Strings_Equal.py

--- leetcode/1790.Check_if_One_String_Swap_Can_Make_Strings_Equal.py
+++ leetcode/1790.Check_if_One_String_Swap_Can_Make_Strings_Equal.py
@@ -1,1 +1,15 @@
-# TODO
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        cnt = 0
+        pos = []
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                pos.append(i)
+        if len(pos) == 0:
+            return True
+        if len(pos) != 2:
+            return False
+        return s1[pos[0]] == s2[pos[1]] and s1[pos[1]] == s2[pos[0]]
+        
