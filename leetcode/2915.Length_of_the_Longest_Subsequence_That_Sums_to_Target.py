9be3d0c7011fd9a60881e34ff23f1ac2c312017b
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-07-20 22:47:45 +0900

    Update 2915.Length_of_the_Longest_Subsequence_That_Sums_to_Target.py

--- leetcode/2915.Length_of_the_Longest_Subsequence_That_Sums_to_Target.py
+++ leetcode/2915.Length_of_the_Longest_Subsequence_That_Sums_to_Target.py
@@ -1,3 +1,14 @@
 class Solution:
     def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        dp = [-1] * (target + 1)
+        dp[0] = 0
+        for i in range(n):
+            tmp = [-1] * (target + 1)
+            for j in range(target + 1):
+                if (j >= nums[i]) and (dp[j - nums[i]] != -1):
+                    tmp[j] = dp[j - nums[i]] + 1
+            for j in range(target + 1):
+                dp[j] = max(dp[j], tmp[j])
         
+        return dp[target]
