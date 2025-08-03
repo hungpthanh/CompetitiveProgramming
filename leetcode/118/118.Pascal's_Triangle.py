bb133fcd80585e01507050af52ef2c39589facc1
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-08-03 21:17:09 +0900

    AC 118

--- leetcode/118/118.Pascal's_Triangle.py
+++ leetcode/118/118.Pascal's_Triangle.py
@@ -1,3 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        results = [[1]]
+        for numRow in range(1, numRows):
+            row = [1]
+            for j in range(1, numRow):
+                row.append(results[-1][j - 1] + results[-1][j])
+            row.append(1)
+            results.append(row)
+        return results
