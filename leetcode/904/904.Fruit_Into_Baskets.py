12bb54ff1a6e833f530c3cc75a2f6b274da13ecf
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-08-06 21:52:05 +0900

    AC 904

--- leetcode/904/904.Fruit_Into_Baskets.py
+++ leetcode/904/904.Fruit_Into_Baskets.py
@@ -1,1 +1,18 @@
-# TODO
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        n = len(fruits)
+        m = 0
+        res = 0
+        st = 0
+        cnt = defaultdict(int)
+        for i in range(n):
+            cnt[fruits[i]] += 1
+            if cnt[fruits[i]] == 1:
+                m += 1
+            while m > 2:
+                cnt[fruits[st]] -= 1
+                if cnt[fruits[st]] == 0:
+                    m -= 1
+                st = st + 1
+            res = max(res, i - st + 1)
+        return res
