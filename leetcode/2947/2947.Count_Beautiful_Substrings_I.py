f73360fe950822dbeaf22ec74c50cb2212c577ab
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-08-08 22:58:44 +0900

    AC 2947

--- leetcode/2947/2947.Count_Beautiful_Substrings_I.py
+++ leetcode/2947/2947.Count_Beautiful_Substrings_I.py
@@ -1,1 +1,14 @@
-# TODO
+class Solution:
+    def beautifulSubstrings(self, s: str, k: int) -> int:
+        n = len(s)
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        counting = [0] * (n + 1)
+        results = 0
+        for i in range(1, n + 1):
+            counting[i] = counting[i - 1] + (1 if s[i - 1] in vowels else 0)
+            for j in range(i):
+                vowel = counting[i] - counting[j]
+                consonant = i - j - vowel
+                if (vowel == consonant) and (((vowel * vowel) % k) == 0):
+                    results += 1
+        return results
