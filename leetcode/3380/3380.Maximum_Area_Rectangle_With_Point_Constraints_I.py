60458850cdb413571407e24c4b1857c14ff730bb
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-08-20 22:41:47 +0900

    idea 3380

--- leetcode/3380/3380.Maximum_Area_Rectangle_With_Point_Constraints_I.py
+++ leetcode/3380/3380.Maximum_Area_Rectangle_With_Point_Constraints_I.py
@@ -1,4 +1,6 @@
 # Idea: TOC - Math
+# for each point, check all points same row and same col and check the
+# four point. Except these 4 points checking any point inside
 class Solution:
     def maxRectangleArea(self, points: List[List[int]]) -> int:
         
