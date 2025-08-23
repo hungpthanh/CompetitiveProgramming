aeb45c18fce08d8e70b254fc8039b1d905302ee9
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-08-23 22:59:40 +0900

    fix bugs wrong meaning of ys

--- leetcode/3380/3380.Maximum_Area_Rectangle_With_Point_Constraints_I.py
+++ leetcode/3380/3380.Maximum_Area_Rectangle_With_Point_Constraints_I.py
@@ -1,6 +1,3 @@
-# Idea: TOC - Math
-# for each point, check all points same row and same col and check the
-# four point. Except these 4 points checking any point inside
 class Solution:
     def maxRectangleArea(self, points: List[List[int]]) -> int:
         xs = defaultdict(list)
@@ -16,12 +13,13 @@ class Solution:
                 for j in ys[points[idx][1]]:
                     nx = points[i][0] + points[j][0] - points[idx][0]
                     ny = points[i][1] + points[j][1] - points[idx][1]
-                    
+
                     minx = min([points[idx][0], points[i][0], points[j][0], nx])
                     miny = min([points[idx][1], points[i][1], points[j][1], ny])
                     maxx = max([points[idx][0], points[i][0], points[j][0], nx])
                     maxy = max([points[idx][1], points[i][1], points[j][1], ny])
-                    if (nx in xs) and (ny in xs[nx]):
+
+                    if [nx, ny] in points:
                         valid = True
                         for k in range(n):
                             if k != idx and k != i and k != j and points[k] != [nx, ny] and (minx <= points[k][0] <= maxx) and (miny <= points[k][1] <= maxy):
@@ -32,5 +30,3 @@ class Solution:
                             if area > 0:
                                 res = max(res, area)
         return res
-
-        
