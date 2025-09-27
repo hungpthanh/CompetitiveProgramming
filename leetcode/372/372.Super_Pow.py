db65f8d6ff0fdcb5b932d0a8038842e5a0c10118
Pham Thanh Hung <hungthanhpham94@gmail.com>
2025-09-27 22:20:28 +0900

    add divide function

--- leetcode/372/372.Super_Pow.py
+++ leetcode/372/372.Super_Pow.py
@@ -1,1 +1,11 @@
-# TODO
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        def devide(numbers: List[int]):
+            remains = 0
+            ans = []
+            for number in numbers:
+                k = remains * 10 + number
+                c = k // 2
+                ans.append(k // 2)
+                remains = k - c * 2
+            return ans
